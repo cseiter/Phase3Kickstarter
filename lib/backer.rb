a0a133d41fb55d4c148dda0e39a57d8d745dc3f4
chris <cseiter@gmail.com>
2021-07-18 10:29:45 -0400

    backed_projects method created

--- lib/backer.rb
+++ lib/backer.rb
@@ -16,4 +16,10 @@ class Backer
     def back_project(project)
         ProjectBacker.new(project,self)
     end
+
+    def backed_projects
+        project_backers = ProjectBacker.all.select {|pb| pb.backer == self}
+        project_backers.map {|pb| pb.project}
+    end
+
 end
